77a46e93018c03239d2b0be5bc4c8e0108a63751
apr1ori <vladgolenok4@gmail.com>
2025-04-11 11:42:54 +0200

    task 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write your code here
+const tHeadRows = document.querySelectorAll('thead tr');
+const tBodyRows = document.querySelectorAll('tbody tr');
+
+Array.from(tHeadRows).forEach((item) => {
+  const cells = Array.from(item.cells);
+
+  cells.splice(cells.length - 1, 0, cells[1]);
+  item.innerHTML = '';
+
+  for (let i = 0; i < cells.length; i++) {
+    item.innerHTML += `<th>${cells[i].textContent}</th>`;
+  }
+});
+
+Array.from(tBodyRows).forEach((item) => {
+  const cells = Array.from(item.cells);
+
+  cells.splice(cells.length - 1, 0, cells[1]);
+  item.innerHTML = '';
+
+  for (let i = 0; i < cells.length; i++) {
+    item.innerHTML += `<td>${cells[i].textContent}</td>`;
+  }
+});
